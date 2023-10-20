ebd5e304f5537a908fcd85b657fa92e34d16cf62
Sundaram Kumar Jha <jhasundaram@outlook.com>
2023-10-20 20:37:40 +0530

    adding 1st route

---
+++ restful/server.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import express from 'express'
+
+const app = express()
+
+app.get('/greeting', (_req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
