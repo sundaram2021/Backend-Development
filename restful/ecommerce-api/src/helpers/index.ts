720d0da5f93c7e0c48d90ab0744c14d42d6ac632
Sundaram Kumar Jha <jhasundaram@outlook.com>
2023-10-23 00:45:35 +0530

    helpers

---
+++ restful/ecommerce-api/src/helpers/index.ts
@@ -0,0 +1,11 @@
+import crypto from 'crypto';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const SECRET = process.env.SECRET || "secret";
+
+export const random = () => crypto.randomBytes(128).toString('base64');
+
+export const authentication = (salt: string, password: string) => {
+    return crypto.createHmac("sha256", [salt, password].join("/")).update(SECRET).digest("hex");
+}
