b5dcf4269ae8b1838fece25a1b4acf7c04793ea4
Sundaram Kumar Jha <jhasundaram@outlook.com>
2023-10-25 17:37:34 +0530

    starting netflix project

---
+++ projects/netflix-backend/src/server.ts
@@ -0,0 +1,36 @@
+import express from 'express';  
+import cors from 'cors';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+
+dotenv.config();
+const PORT  = process.env.PORT || 3000;
+const URL = process.env.MONGODB_URL || '';
+const app = express();
+
+
+app.use(cors());
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.get('/', (_req, res) => {
+    res.send('Hello World!');
+});
+
+mongoose.Promise = global.Promise;
+mongoose.connect(URL)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log('Error connecting to MongoDB', err);
+    });
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
